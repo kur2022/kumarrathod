560e36c445b1c4aca6794aee0b1efebc35c29fda
kur2022 <112455143+kur2022@users.noreply.github.com>
2025-09-15 17:54:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from telegram import Bot
 
 # ✅ Telegram setup
 bot_token = "7970626014:AAG6QFs0ZWohqkkGaNhJ7P4qkhaJN-UMe74"
-chat_id = "7970626014"
+chat_id = "-1002785266393"
 bot = Bot(token=bot_token)
 
 def send_telegram_alert(message):
