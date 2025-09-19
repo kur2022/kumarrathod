fb560b2592a7068946a834930ff62198f3298ece
kur2022 <112455143+kur2022@users.noreply.github.com>
2025-09-19 20:08:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import os
-
-from fetchers import fetch_chartink_support, fetch_topstock_resistance
-from alerts import connect_smartapi, get_live_price
-from utils import save_signals, calculate_levels, LOG_FILE
-
+from smartapi import SmartConnect
 from smartapi import SmartWebSocket
+from utils import connect_smartapi, get_live_price
+from fetchers import fetch_chartink_support, fetch_topstock_resistance
+from utils import save_signals, calculate_levels, LOG_FILE
 
 # Store ticks in session state
 if "tick_data" not in st.session_state:
